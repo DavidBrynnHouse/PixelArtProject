a624006421892675cc62bd38fc959bde4ca7ef33
David House <Dahous1@umbc.edu>
2016-08-19 20:40:05 -0400

    points

---
+++ Assets/Code/PointStar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointStar : MonoBehaviour
+{
+	public GameObject Effect;
+	public int PointsToAdd = 10;
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+
+	}
+
+}
